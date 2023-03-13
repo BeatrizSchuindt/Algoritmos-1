15cedaef5376303590e242f6f5ebe81578da90ad
Ana Beatriz Schuindt <beatriz.schuindt@estudante.ifmt.edu.br>
2023-03-13 10:34:10 -0400

    Criei o exercício 8 da lista 5 do dia 27/02 em C
    Enunciado do programa:
    "8) Escreva um algoritmo que leia dois números reais e escreva o maior deles. Se os números forem iguais, qualquer um deles por ser escrito (pois são os mesmos)."

---
+++ lista5-27-02-papel/exercicio8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    float num1, num2;
+
+    printf ("Digite o primeiro número: ");
+    scanf ("%f", &num1);
+    printf ("\nDigite o segundo número: ");
+    scanf ("%f", &num2);
+
+    if (num1 == num2) {
+        	printf ("\nOs números são iguais: %.1f", num1);
+    } else {
+        	 if (num1 > num2) {
+            	printf ("\nO maior número é: %.1f", num1);
+         	} else {
+           		printf ("\nO maior número é: %.1f", num2);
+        	}
+    }
+    return 0;
+}
